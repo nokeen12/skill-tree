6ab9021da58d8757e570f4be5203156bff5c3cb4
Cordre <cordrepayne@gmail.com>
2023-02-28 21:37:19 -0600

    add react router dom and create working home and error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
 
+import { Routes, Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import Errorpage from './pages/Errorpage';
+
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='*' element={<Errorpage/>} />
+      </Routes>
     </div>
   )
 }
